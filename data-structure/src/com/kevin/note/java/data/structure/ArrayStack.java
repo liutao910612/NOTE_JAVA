be23c798574c27972f0239c215e68d1c8acf529a
DESKTOP-FEURCKB\yyt_c <liutao910612@126.com>
2021-03-01 21:14:18 +0800

    链表（上）：如何实现LRU缓存淘汰算法

---
+++ data-structure/src/com/kevin/note/java/data/structure/ArrayStack.java
@@ -0,0 +1,43 @@
+package com.kevin.note.java.data.structure;
+
+/**
+ * 使用数组来实现栈
+ *
+ * @Author:Kevin
+ * @Date:Created in 20:34 2021/3/1
+ */
+public class ArrayStack {
+    private String[] items;  //存储栈的数据
+    private int count;  //栈中元素的个数
+    private int n;  //栈的大小
+
+    public ArrayStack(int n) {
+        this.items = new String[n];
+        this.n = n;
+        this.count = 0;
+    }
+
+    public boolean push(String item) {
+
+        //栈空间已满
+        if (count == n) {
+            return false;
+        }
+
+        items[count] = item;
+        count++;
+        return true;
+    }
+
+    public String pop() {
+
+        //栈中没有任何元素
+        if (count == 0) {
+            return null;
+        }
+
+        String tmp = items[count - 1];
+        count--;
+        return tmp;
+    }
+}
