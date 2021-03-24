f327a248c701e37b7c9e3a46560d6e9b79ef8df5
DESKTOP-FEURCKB\yyt_c <liutao910612@126.com>
2021-03-24 21:41:28 +0800

    tree

--- data-structure/src/com/kevin/note/java/data/structure/tree/BinarySearchTree.java
+++ data-structure/src/com/kevin/note/java/data/structure/tree/BinarySearchTree.java
@@ -23,6 +23,76 @@ public class BinarySearchTree {
         return null;
     }
 
+    public void insert(int data) {
+        if (tree == null) {
+            tree = new Node(data);
+            return;
+        }
+
+        Node p = tree;
+        while (p != null) {
+            if (data > p.data) {
+                if (p.right == null) {
+                    p.right = new Node(data);
+                    return;
+                }
+                p = p.right;
+            } else {
+                if (p.left == null) {
+                    p.left = new Node(data);
+                    return;
+                }
+                p = p.left;
+            }
+        }
+    }
+
+    public void delete(int data) {
+        Node p = tree;
+        Node pp = null;
+        while (p != null && p.data != data) {
+            pp = p;
+            if (data > p.data) {
+                p = p.right;
+            } else {
+                p = p.left;
+            }
+        }
+
+        //Don't find
+        if (p == null) {
+            return;
+        }
+
+        //Node will be deleted have two nodes
+        if (p.left != null && p.right != null) {
+            Node minP = p.left;
+            Node minPP = p;
+            while (minP.left != null) {
+                minPP = minP;
+                minP = minP.left;
+            }
+            p.data = minP.data;
+            p = minP;
+            pp = minPP;
+        }
+
+        Node child = null;
+        if (p.left != null) {
+            child = p.left;
+        } else if (p.right != null) {
+            child = p.right;
+        }
+        if (pp == null) {
+            tree = child;
+        } else if (pp.left == p) {
+            pp.left = child;
+        } else {
+            pp.right = child;
+        }
+    }
+
+
     public static class Node {
         private int data;
         private Node left;
